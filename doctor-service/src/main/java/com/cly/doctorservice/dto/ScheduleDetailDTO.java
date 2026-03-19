d95e900a00002cad9d9e914c6540957e1de74d0e
future <1961163997@qq.com>
2026-03-19 09:44:26 +0800

    版本5

---
+++ doctor-service/src/main/java/com/cly/doctorservice/dto/ScheduleDetailDTO.java
@@ -0,0 +1,23 @@
+package com.cly.doctorservice.dto;
+
+import lombok.Data;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+public class ScheduleDetailDTO {
+
+    // 排班信息
+    private Long scheduleId;
+    private LocalDate workDate;
+    private Integer availableNum;
+    private Integer scheduleStatus;
+
+    // 医生信息
+    private Long docId;
+    private String docName;
+    private String docTitle;
+    private BigDecimal docFee;
+    private Long deptId;
+
+}
